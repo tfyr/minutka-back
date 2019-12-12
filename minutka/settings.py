8ed8f1f5f7a0f78451284c2402eac73968d62dfd
nash <nash34@gmail.com>
2019-12-13 04:27:05 +0500

    init

--- minutka/settings.py
+++ minutka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+p1m1a%k@iv90gz&=xsv$gz-08ns(39$uzb!jmz8vnlvz82xf9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['minutka.9733.ru']
 
 
 # Application definition
